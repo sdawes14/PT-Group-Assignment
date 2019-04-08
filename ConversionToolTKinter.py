f7e5a7505108b58dce02bdc9f987452acf65ea14
sdawes14 <48328454+sdawes14@users.noreply.github.com>
2019-04-07 21:17:28 -0500

    Update ConversionToolTKinter.py

--- ConversionToolTKinter.py
+++ ConversionToolTKinter.py
@@ -4,10 +4,10 @@ import tkinter
 app = tkinter.Tk()
 v = tkinter.IntVar()
 v.set(1)
-app.geometry('480x300')
-app.title("Conversion Tool")
+app.geometry('480x300') #Window Dimensions
+app.title("Conversion Tool")	#Window Title
 
-def convert(sv,v,ot):
+def convert(sv,v,ot): #Conversion Formulas Functions
 	if (sv.get().isdigit()):
 		print("v.get() :"+str(v.get())+" | sv.get(): "+str(sv.get()))
 		if v.get() == 0:
@@ -28,7 +28,7 @@ def convert(sv,v,ot):
 			ot.set(round(float(sv.get())*2.20462,1)) #Kilograms to Pounds
 		elif v.get() == 8:
 			ot.set(round(float(sv.get())*0.453592,1)) #Pounds to Kilograms	
-
+#Radiobutton Dimensions and Add-ons
 L1=tkinter.Label(app, text="Choose your conversion:",justify="left",  padx = 20).grid(row=0, column=0, columnspan=1, sticky=tkinter.W+tkinter.E)
 RB1=tkinter.Radiobutton(app, text="Celsius to Fahrenheit",justify="left",padx = 20, variable=v, value=1).grid(row=1, column=0, columnspan=1,sticky=tkinter.W+tkinter.E)
 RB2=tkinter.Radiobutton(app, text="Fahrenheit to Celsius",justify="left",padx = 20, variable=v, value=2).grid(row=2, column=0, columnspan=1,sticky=tkinter.W+tkinter.E)
@@ -38,17 +38,17 @@ RB5=tkinter.Radiobutton(app, text="JMD to U.S. Dollars",justify = "left", padx =
 RB6=tkinter.Radiobutton(app, text="U.S. Dollars to JMD",justify = "left", padx = 20, variable=v, value=6).grid(row=6, column=0, columnspan=1, sticky=tkinter.W+tkinter.E)
 RB7=tkinter.Radiobutton(app, text="Kilograms to Pounds",justify = "left", padx = 20, variable=v, value=7).grid(row=7, column=0, columnspan=1, sticky=tkinter.W+tkinter.E)
 RB8=tkinter.Radiobutton(app, text="Pounds to Kilograms",justify = "left", padx = 20, variable=v, value=8).grid(row=8, column=0, columnspan=1, sticky=tkinter.W+tkinter.E)
-
+#Conversion Values Placed in this box with dimensions
 L2=tkinter.Label(app, text="Value to convert:",justify = "left",  padx = 20).grid(row=10, column=1, columnspan=1, sticky=tkinter.W+tkinter.E)
-
+#Functions Calls
 ot = tkinter.StringVar()
 sv = tkinter.StringVar()
 sv.trace("w", lambda name, index, mode, sv=sv: convert(sv,v,ot))
 
 
-
+#Entry label and box 
 E1=tkinter.Entry(app, width=4,textvariable=sv).grid(row=4, column=3, columnspan=1, sticky=tkinter.W+tkinter.E)
 L3=tkinter.Label(app, text="Converted Value:", fg="red", padx = 20).grid(row=11, column=0, columnspan=4, sticky=tkinter.W+tkinter.E)
 E2=tkinter.Entry(app, width=4,state='disabled',textvariable=ot).grid(row=11, column=0, columnspan=4, sticky=tkinter.W+tkinter.E)
 
-app.mainloop()
+app.mainloop()  #Loop
